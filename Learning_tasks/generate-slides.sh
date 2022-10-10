1f3526f7f1c9b4bfaa751601972007754dc686fa
Franck Silvestre <franck.silvestre@gmail.com>
2022-10-10 11:16:01 +0200

    Retour tests en cours

--- Learning_tasks/generate-slides.sh
+++ Learning_tasks/generate-slides.sh
@@ -2,4 +2,9 @@ bundle exec asciidoctor-revealjs \
 -r asciidoctor-diagram \
 -a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2 \
 RetourUS.adoc \
--o RetourUS.slides.html
+-o RetourUS.slides.html \
+& bundle exec asciidoctor-revealjs \
+-r asciidoctor-diagram \
+-a revealjsdir=https://cdn.jsdelivr.net/npm/reveal.js@3.9.2 \
+RetourAT.adoc \
+-o RetourAT.slides.html
